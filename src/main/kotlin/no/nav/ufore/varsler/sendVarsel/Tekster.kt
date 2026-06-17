69398e0f1f6add47530207ea5dca3410fb48e28f
André Mossige <andre.mossige@nav.no>
2026-06-17 12:30:53 +0200

    Fiks test med midlertidig, men gyldig tekst

--- src/main/kotlin/no/nav/ufore/varsler/sendVarsel/Tekster.kt
+++ src/main/kotlin/no/nav/ufore/varsler/sendVarsel/Tekster.kt
@@ -1,5 +1,6 @@
 package no.nav.ufore.varsler.sendVarsel
 
-val UNGE_MED_UFORE_SMS = "Hei! Visste du at Nav kan hjelpe deg med å se på mulighetene dine for jobb og utdanning? Du kan logge inn på Nav for å lese mer om dette frivillige tilbudet. Hilsen Nav"
+// TODO: Riktige tekster
+val UNGE_MED_UFORE_SMS = "Hei! Nav kan hjelpe deg å se mulighetene dine for jobb og utdanning. Logg inn på Nav for mer info om tilbudet. Hilsen Nav"
 val UNGE_MED_UFORE_VARSEL = "Dine muligheter - Vi kan hjelpe deg med jobb, studier og økonomisk rådgiving. "
 
--- src/test/kotlin/no/nav/ufore/varsler/sendVarsel/SendVarselProducerTest.kt
+++ src/test/kotlin/no/nav/ufore/varsler/sendVarsel/SendVarselProducerTest.kt
@@ -1,9 +1,11 @@
 package no.nav.ufore.varsler.sendVarsel
 
+import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
 import org.springframework.kafka.core.KafkaTemplate
 import java.util.*
+import java.util.concurrent.CompletableFuture
 
 class SendVarselProducerTest {
 
@@ -20,6 +22,10 @@ class SendVarselProducerTest {
 
     @Test
     fun `sendBeskjed skal ikke feile`() {
+        every {
+            kafkaTemplate.send(any(), any(), any())
+        } returns CompletableFuture.completedFuture(mockk(relaxed = true))
+
         val request = SendVarselRequest(
             varselId = UUID.randomUUID().toString(),
             ident = "12345678901",
