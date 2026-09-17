f343f5c68d37c88f5919cb58d2b78c9e816a1f6a
André Mossige <andre.mossige@nav.no>
2026-09-17 15:26:18 +0200

    Oppdater varseltekst

--- src/main/kotlin/no/nav/ufore/varsler/sendVarsel/Tekster.kt
+++ src/main/kotlin/no/nav/ufore/varsler/sendVarsel/Tekster.kt
@@ -1,4 +1,4 @@
 package no.nav.ufore.varsler.sendVarsel
 
 val UNGE_MED_UFORE_SMS = "Hei! Visste du at Nav kan hjelpe deg med å se på mulighetene dine for jobb og utdanning? Logg inn på Nav for å lese om dette frivillige tilbudet. Hilsen Nav"
-val UNGE_MED_UFORE_VARSEL = "Dine muligheter - Vi kan hjelpe deg med jobb, studier og økonomisk rådgiving. "
+val UNGE_MED_UFORE_VARSEL = "Dine muligheter - Vi kan hjelpe deg med jobb, utdanning og økonomisk rådgiving. "
